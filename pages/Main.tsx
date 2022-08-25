79180408152fcd345f10eaaccdda05fb2bbbe6e5
Huy Tú <danghuytu.thd2006@gmail.com>
2022-08-25 11:57:50 +0700

    use useCallback

--- pages/Main.tsx
+++ pages/Main.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React,{memo} from "react";
 
 const Main = ({ count }: { count: number }) => {
+    console.log('ndnasjdnjsa')
   return (
       <div className="flex justify-center mt-10 ">Count:<p className="text-red-600 font-bold">{count}</p></div>
   );
 };
 
-export default Main;
+export default memo(Main);
